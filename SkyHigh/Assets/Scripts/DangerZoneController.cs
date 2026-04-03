4a52ff1c1fa2a215c3ec1eb9cf377379ab08cd9c
fratyaldiz <firatylz2004@hotmail.com>
2026-04-03 16:53:28 +0300

    feat: add DangerZoneController.cs and FlightExamManager.cs

--- SkyHigh/Assets/Scripts/DangerZoneController.cs
+++ SkyHigh/Assets/Scripts/DangerZoneController.cs
@@ -1,6 +1,66 @@
+// DangerZoneController.cs
 using UnityEngine;
+using System.Collections; // i add this because timer need this
 
-public class DangerZoneController
+public class DangerZoneController : MonoBehaviour
 {
+    [SerializeField] private FlightExamManager examManager;
+    //[SerializeField] private MissileLauncher missileLauncher;
+    [SerializeField] private float missileDelay = 5f;
     
+    private Coroutine activeCountdown;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        // TODO: confirm the Player tag
+        // we check if it is player ship
+        if (collision.CompareTag("Player"))
+        {
+            // TODO: push the warning message "Entered a Dangerous Zone!" to the HUD
+            // manager show red text in screen
+            if (examManager != null)
+            {
+                examManager.EnterDangerZone();
+            }
+
+            // TODO: start the delayed missile launch countdown
+            // we start 5 second timer for missile
+            activeCountdown = StartCoroutine(MissileTimer(collision.transform));
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        // TODO: confirm the Player tag
+        
+        if (collision.CompareTag("Player")) // we check player escape?
+        {
+            // TODO: cancel any pending launch countdown
+            // timer stop
+            if (activeCountdown != null)
+            {
+                StopCoroutine(activeCountdown);
+                activeCountdown = null;
+            }
+
+            // TODO: destroy the active missile and clear the HUD warning
+            // manager hide red text
+            if (examManager != null)
+            {
+                examManager.ExitDangerZone();
+            }
+
+            // TODO: missile is destroy now
+           
+        }
+    }
+
+    // this is timer routine. it wait then shoot.
+    private IEnumerator MissileTimer(Transform playerTransform)
+    {
+        // wait 5 second
+        yield return new WaitForSeconds(missileDelay);
+        
+        // time is finish, shoot missile to player!
+    }
 }
---
+++ SkyHigh/Assets/Scripts/FlightExamManager.cs
@@ -0,0 +1,48 @@
+// FlightExamManager.cs
+using UnityEngine;
+using TMPro;
+
+public class FlightExamManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text statusText;
+    [SerializeField] private TMP_Text missionText;
+    
+    private bool hasTakenOff = false;
+    private bool threatCleared = false;
+    private bool missionComplete = false;
+
+    private void Start()
+    {
+        // game start, we hide warning text
+        if (statusText != null)
+        {
+            statusText.gameObject.SetActive(false);
+        }
+    }
+
+    public void EnterDangerZone()
+    {
+        // TODO: update the mission state and HUD
+        // player go inside danger place. we show red text
+        if (statusText != null)
+        {
+            statusText.gameObject.SetActive(true);
+            statusText.text = "Entered a Dangerous Zone!";
+        }
+        
+        // danger is start
+        threatCleared = false;
+    }
+
+    public void ExitDangerZone()
+    {
+        // TODO: mark the threat as cleared and refresh the HUD
+        // player escape from danger. we hide text again.
+        if (statusText != null)
+        {
+            statusText.gameObject.SetActive(false);
+        }
+        
+        threatCleared = true;
+    }
+}
